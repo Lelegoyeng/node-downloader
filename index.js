160fd46948641cabb8754e41363f707eeefafe1a
lelegoyeng <lelegoyengs@gmail.com>
2023-12-05 11:09:30 +0700

    Update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Downloader = require("nodejs-file-downloader");
+const inquirer = require('inquirer');
 
-(async () => {
 
+const downloadFiles = async (link) => {
     const downloader = new Downloader({
-        url: "http://212.183.159.230/200MB.zip",
+        // url: "http://212.183.159.230/200MB.zip",
+        url: link,
         directory: "./downloads",
         onProgress: function (percentage, chunk, remainingSize) {
 
@@ -12,11 +14,35 @@ const Downloader = require("nodejs-file-downloader");
             // console.log("Remaining bytes: ", remainingSize);
         },
     });
+
     try {
         const { filePath, downloadStatus } = await downloader.download();
-
         console.log("All done");
     } catch (error) {
         console.log("Download failed", error);
     }
-})();
+}
+
+
+inquirer
+    .prompt([
+        {
+            name: 'action',
+            message: 'Masukkan Link Download Anda',
+        },
+    ])
+    .then(async (answers) => {
+        if (answers.action) {
+            console.log(`Downloding: ${answers.action}.`);
+            downloadFiles(answers.action)
+        } else {
+            console.log(`Link Kosong`)
+        }
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("Prompt tidak dapat dirender di lingkungan saat ini.");
+        } else {
+            console.log("Terjadi kesalahan lain: ", error);
+        }
+    });
